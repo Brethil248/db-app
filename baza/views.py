a13e0d9433f85fdbc0e68258e90dcb2d0684ee89
Stork <brethil248@gmail.com>
2016-06-09 11:40:07 +0200

    Templates modified to show contents of Posts model

--- baza/views.py
+++ baza/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'baza\post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'baza\post_list.html', {'posts': posts})
+
+    # render pobiera dane z apki sieciowej (nowe elementy)
+    # 'posts': posts określa z czego korzysta template
